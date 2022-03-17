04e3f6b0247b2739d3c98305fca9492ecf38d69f
Reuben T Allik <reubenallik@gmail.com>
2022-03-17 17:59:32 -0400

    settings change

--- server/workout_tracker/settings.py
+++ server/workout_tracker/settings.py
@@ -11,16 +11,24 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Access env file
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rbn6_!x%4i2s*d1q96$ie89e*+$1wgwyy^*4y(z!p2g_tu4pak'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
