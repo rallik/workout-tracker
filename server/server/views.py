75c4b449fe10ab15ab66b645172fdf1a13a321cc
Reuben T Allik <reubenallik@gmail.com>
2022-03-18 14:06:59 -0400

    os to pathlib, added serializers.py

--- server/server/views.py
+++ server/server/views.py
@@ -1,4 +1,8 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(["GET"])
 def test_resp(request, *args, **kwargs):
-    return JsonResponse({'message': 'Hello world'})
+    return Response(request)
--- server/workout_tracker/settings.py
+++ server/workout_tracker/settings.py
@@ -10,18 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, PurePath
 import environ
-import os
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parents[1]
 
 # Access env file
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(PurePath(BASE_DIR).joinpath('.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'server'
 ]
 
 MIDDLEWARE = [
